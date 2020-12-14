83f247cba6bcccdd1250b4bbb5160800f73054f2
Maurice Loskyll <maurice@loskyll.de>
2020-12-14 13:44:26 +0100

    Add separate deploy script for github action macos version

---
+++ .vscode/darwin_scripts/deploy_executable_github_actions.sh
@@ -0,0 +1,6 @@
+rm -r -f deployment/vehicle_dynamics_simulation_darwin
+mkdir -p deployment/vehicle_dynamics_simulation_darwin
+cp "$(bazel info bazel-genfiles)/vehicle_dynamics_simulation" deployment/vehicle_dynamics_simulation_darwin/
+cp vehicle_specifications/mazda_rx8.yaml deployment/vehicle_dynamics_simulation_darwin/
+cd deployment
+zip -r vehicle_dynamics_simulation_darwin.zip vehicle_dynamics_simulation_darwin
